cbe8da2b8f50d475981605c52f83ad8d5ec70d8e
Gabriel <carlosgabriel060178@gmail.com>
2024-05-08 11:54:58 -0300

    arquivo gruntfile.js

---
+++ exercicios/exercicio_grunt/Gruntfile.js
@@ -0,0 +1,26 @@
+const { file, option } = require('grunt')
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'build/styles/main.css': 'src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['less', 'uglify']);
+}
